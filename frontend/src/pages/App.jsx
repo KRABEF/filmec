b2a43ec73e7b1cc35fabaaf148ba2a370af29b7c
Gordey <gordeipuzyryov@yandex.ru>
2025-10-01 12:14:21 +0500

    Создали header и footer

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,5 +1,21 @@
+import { Footer } from "../components/footer";
+import { Header } from "../components/header";
+
 export default function App() {
-  return <h1 className="text-amber-700">Hello world!</h1>;
+  // return <h1 className="text-amber-700">Hello world!</h1>;
+  return <div>
+    <Header>
+
+    </Header>
+    
+    <main>
+      
+    </main>
+
+    <Footer>
+
+    </Footer>
+  </div>
 }
 
 
