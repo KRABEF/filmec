3b2bb6f92acbfab898ff9cd3c17fd4f98ea4cfc0
khalazar <khalazar@MacBook-Air-khalazar.local>
2025-10-02 10:48:16 +0500

    testo

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -2,88 +2,91 @@ import { ContainerMain } from '../components/containerMain';
 import { Header } from '../components/header';
 
 export default function App() {
-	return (
-		<div className="flex flex-col min-h-screen">
-			<Header />
-			<main className="flex flex-grow">
-				<div className="w-90 sticky top-0 bg-neutral-900 p-4">Левое меню</div>
-				<div className="flex-grow overflow-auto p-4 h-[calc(100vh-100px)]">
-					<ContainerMain
-						movies={[
-							{
-								id: 1,
-								name: 'Колбаса',
-								release_date: '2025',
-								genres: ['комедия', 'ужастик'],
-								duration_movie: '2 мин.',
-								rating: 5.3,
-								ageLimit: '18+',
-								poster: '/25630addd03346e5b2fccdeb32bf.jpg',
-							},
-							{
-								id: 1,
-								name: 'Колбаса',
-								release_date: '2025',
-								genres: ['комедия', 'ужастик'],
-								duration_movie: '2 мин.',
-								rating: 5.3,
-								ageLimit: '18+',
-								poster: '/0f1ddf5387cfbfd31303003237bd150a.jpg',
-							},
-							{
-								id: 1,
-								name: 'Колбаса',
-								release_date: '2025',
-								genres: ['комедия', 'ужастик'],
-								duration_movie: '2 мин.',
-								rating: 5.3,
-								ageLimit: '18+',
-								poster: '/25630addd03346e5b2fccdeb32bf.jpg',
-							},
-							{
-								id: 1,
-								name: 'Колбаса',
-								release_date: '2025',
-								genres: ['комедия', 'ужастик'],
-								duration_movie: '2 мин.',
-								rating: 5.3,
-								ageLimit: '18+',
-								poster: '/25630addd03346e5b2fccdeb32bf.jpg',
-							},
-							{
-								id: 1,
-								name: 'Колбаса',
-								release_date: '2025',
-								genres: ['комедия', 'ужастик'],
-								duration_movie: '2 мин.',
-								rating: 5.3,
-								ageLimit: '18+',
-								poster: '/25630addd03346e5b2fccdeb32bf.jpg',
-							},
-							{
-								id: 1,
-								name: 'Колбаса',
-								release_date: '2025',
-								genres: ['комедия', 'ужастик'],
-								duration_movie: '2 мин.',
-								rating: 5.3,
-								ageLimit: '18+',
-								poster: '/25630addd03346e5b2fccdeb32bf.jpg',
-							},
-							{
-								id: 1,
-								name: 'The Witcher: Nightmare of the Wolf',
-								release_date: '2025',
-								genres: ['комедия', 'ужастик'],
-								duration_movie: '2 мин.',
-								rating: 5.3,
-								ageLimit: '18+',
-								poster: '/25630addd03346e5b2fccdeb32bf.jpg',
-							},
-						]}
-					/>
-				</div>
-			</main>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-grow">
+        <div className="w-90 sticky top-0 bg-neutral-900 p-4">Левое меню
+          <leftMenu>Меню</leftMenu>
+			<Button>Отправить</Button>
 		</div>
-	);
+        <div className="flex-grow overflow-auto p-4 h-[calc(100vh-100px)]">
+          <ContainerMain
+            movies={[
+              {
+                id: 1,
+                name: 'Колбаса',
+                release_date: '2025',
+                genres: ['комедия', 'ужастик'],
+                duration_movie: '2 мин.',
+                rating: 5.3,
+                ageLimit: '18+',
+                poster: '/25630addd03346e5b2fccdeb32bf.jpg',
+              },
+              {
+                id: 1,
+                name: 'Колбаса',
+                release_date: '2025',
+                genres: ['комедия', 'ужастик'],
+                duration_movie: '2 мин.',
+                rating: 5.3,
+                ageLimit: '18+',
+                poster: '/0f1ddf5387cfbfd31303003237bd150a.jpg',
+              },
+              {
+                id: 1,
+                name: 'Колбаса',
+                release_date: '2025',
+                genres: ['комедия', 'ужастик'],
+                duration_movie: '2 мин.',
+                rating: 5.3,
+                ageLimit: '18+',
+                poster: '/25630addd03346e5b2fccdeb32bf.jpg',
+              },
+              {
+                id: 1,
+                name: 'Колбаса',
+                release_date: '2025',
+                genres: ['комедия', 'ужастик'],
+                duration_movie: '2 мин.',
+                rating: 5.3,
+                ageLimit: '18+',
+                poster: '/25630addd03346e5b2fccdeb32bf.jpg',
+              },
+              {
+                id: 1,
+                name: 'Колбаса',
+                release_date: '2025',
+                genres: ['комедия', 'ужастик'],
+                duration_movie: '2 мин.',
+                rating: 5.3,
+                ageLimit: '18+',
+                poster: '/25630addd03346e5b2fccdeb32bf.jpg',
+              },
+              {
+                id: 1,
+                name: 'Колбаса',
+                release_date: '2025',
+                genres: ['комедия', 'ужастик'],
+                duration_movie: '2 мин.',
+                rating: 5.3,
+                ageLimit: '18+',
+                poster: '/25630addd03346e5b2fccdeb32bf.jpg',
+              },
+              {
+                id: 1,
+                name: 'The Witcher: Nightmare of the Wolf',
+                release_date: '2025',
+                genres: ['комедия', 'ужастик'],
+                duration_movie: '2 мин.',
+                rating: 5.3,
+                ageLimit: '18+',
+                poster: '/25630addd03346e5b2fccdeb32bf.jpg',
+              },
+            ]}
+          />
+        </div>
+      </main>
+    </div>
+  );
 }
