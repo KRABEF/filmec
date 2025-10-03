ec6fafccf6447ab2bf34e51d2bcd8b5e7833860c
KRABEF <sormachev.vsevolod@gmail.com>
2025-10-03 11:40:38 +0500

    Логирование запросов

--- backend/root/main.js
+++ backend/root/main.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 
 const express = require('express');
+const requestLogger = require('../middlewares/requestLogger.js');
 
 const app = express();
-const PORT = process.env.DB_PORT || 3000;
+const PORT = process.env.BACK_PORT || 3000;
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.get('/', (req, res) => {
-	res.json({ name: 'Keril' });
+  res.json({ name: 'Яч', age: 99 });
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
