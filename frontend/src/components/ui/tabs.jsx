e2b37e63da194f806e9d8ab5f307235bf4f59101
beetleBA <sokolova4506@yandex.ru>
2025-10-11 12:22:06 +0500

    Добавлен компонент ui Tabs

---
+++ frontend/src/components/ui/tabs.jsx
@@ -0,0 +1,42 @@
+import { useState, useContext, createContext } from 'react';
+
+const TabsContext = createContext(undefined);
+
+export const Tabs = {
+  Root: ({ defaultValue, children }) => {
+    const [activeValue, setActiveValue] = useState(defaultValue);
+    return (
+      <TabsContext.Provider value={{ activeValue, setActiveValue }}>
+        <div>{children}</div>
+      </TabsContext.Provider>
+    );
+  },
+
+  List: ({ children }) => {
+    return <div className="flex gap-2 border-b-1 border-neutral-600">{children}</div>;
+  },
+
+  Trigger: ({ value, children }) => {
+    const { activeValue, setActiveValue } = useContext(TabsContext);
+    const isActive = activeValue === value;
+
+    return (
+      <button
+        className={`py-2 px-3 cursor-pointer relative transition-all duration-100 ${
+          isActive ? 'text-white' : 'text-neutral-400 '
+        }`}
+        onClick={() => setActiveValue(value)}
+      >
+        {isActive && (
+          <div className="absolute bottom-0 left-0 right-0 h-[2px] bg-orange-500 -mb-[1px] z-10" />
+        )}
+        <div className="hover:bg-neutral-700 py-1 px-3 rounded-sm">{children}</div>
+      </button>
+    );
+  },
+
+  Content: ({ value, children }) => {
+    const context = useContext(TabsContext);
+    return context.activeValue === value ? <div>{children}</div> : null;
+  },
+};
