78d6cd58922567dbb539b0c4c2860bd74e86d0c3
caxarok9009 <stas545gmail@mail.ru>
2025-12-25 19:01:38 +0500

    uploads

--- backend/root/main.js
+++ backend/root/main.js
@@ -25,7 +25,11 @@ app.use('/api/films', require('../route/filmsRoute.js'));
 // users
 const { authenticateToken, globalGuard } = require('../middlewares/auth');
 app.use('/api/users', authenticateToken, globalGuard, require('../route/usersRoute'));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(
+  '/uploads',
+  express.static(path.join(__dirname, '..', 'uploads'))
+);
+
 
 app.listen(BACK_PORT, () => {
   console.log(`Server is running on port ${BACK_PORT}`);
